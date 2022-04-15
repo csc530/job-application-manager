0b0078fac86d9869ec559d03ddf9ca09ff4f9bc4
csc530 <cscousin@lakeheadu.ca>
2022-04-15 14:23:14 -0400

    test(production): :test_tube: Test if indeed-job-scraper is working

--- routes/indeed.js
+++ routes/indeed.js
@@ -4,6 +4,30 @@ const passport = require('passport');
 
 const Application  = require('../models/application');
 
+// *  websocket package for server side websocket
+const{Server} = require('ws');
+
+const{config, getJobsList} = require('indeed-job-scraper');
+config['verbose'] = true;  //to deliver information about current processing
+config['base-URL']  = 'https://ca.indeed.com';
+
+const wss = new Server({port: 8080});
+wss.on('connection', function connection(ws) {
+	ws.on('message', function message(msg) {
+		const title = msg.toString();
+		const searchParams =
+		{
+			sort: 'date',
+		};
+		if(title)
+			searchParams.queryTitle = title;
+		getJobsList(searchParams)
+			.then(jobs => ws.send(JSON.stringify(jobs)));
+	});
+});
+
+
+
 /* GET  jobs */
 router.get('/', (req, res, next) => {
 	const searchTitle = req.query.search;
